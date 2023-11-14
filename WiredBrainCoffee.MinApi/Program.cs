9217e3080642520b3bb16e4fe95c93650955c85e
Alex Wolf <alexwolf@mcirosoft.com>
2023-11-13 22:24:38 -0500

    fix

--- WiredBrainCoffee.MinApi/Program.cs
+++ WiredBrainCoffee.MinApi/Program.cs
@@ -1,5 +1,3 @@
-using System.Diagnostics;
-using System.Text.Json.Serialization;
 using WiredBrainCoffee.MinApi.Services;
 using WiredBrainCoffee.MinApi.Services.Interfaces;
 using WiredBrainCoffee.Models;
